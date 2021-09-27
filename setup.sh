6e676d2934ed40c2d855c7af7a197e448532bb8d
Mountagha <muntaghaba@gmail.com>
2021-09-26 23:20:48 -0400

    fix sym link bugs

--- setup.sh
+++ setup.sh
@@ -10,10 +10,10 @@ for dir in `ls`; do
 done
 # can't iterate for the following files so create the link one at a time
 cd ./bin
-ln -s ../lib/.i686/cgen refcgen 
-ln -s ../lib/.i686/lexer reflexer 
-ln -s ../lib/.i686/parser refparser 
-ln -s ../lib/.i686/semant refsemant
+ln -s "$base_dir/lib/.i686/cgen" "$base_dir/bin/refcgen" 
+ln -s "$base_dir/lib/.i686/lexer" "$base_dir/bin/reflexer" 
+ln -s "$base_dir/lib/.i686/parser" "$base_dir/bin/refparser" 
+ln -s "$base_dir/lib/.i686/semant" "$base_dir/bin/refsemant" 
 
 ln -s /usr/class/cs143/cool ~/cool
 
